78001ccb63453b4a2b98a292836fc68502f5a0ed
ahhzaky <siahmadzaky@gmail.com>
2020-09-06 23:42:22 +0700

    Done Footer and card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Home from "./Home/Home";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
 
       {/* Home */}
       {/* Header */}
