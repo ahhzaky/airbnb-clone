af6c06fca405117086cec2d5ca9da59388f1ff69
ahhzaky <siahmadzaky@gmail.com>
2020-09-07 00:23:42 +0700

    add searchpage file

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 import Home from "./Home/Home";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage/SearchPage";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            {/* /Search */}
+            <SearchPage />
+          </Route>
 
-      {/* Home */}
-      {/* Header */}
-      {/* Bannner */}
-      {/* search */}
+          <Route path="/">
+            {/* / */}
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* cards */}
-
-      {/* Footer */}
-
-      {/* search page */}
-      {/* Header */}
-      {/* .... */}
+        <Footer />
+      </Router>
     </div>
   );
 }
